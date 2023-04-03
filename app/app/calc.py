2bed6d8115168ec2c5a5a6094860ba51f9530858
Ruslan Lakazov <lakazov77@gmail.com>
2023-04-03 16:17:23 +0300

    Installed PostgreSQL database adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+calculator functions
+"""
+
+
+def add(x, y):
+    """Add x & y and return results."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return results."""
+    return x - y
